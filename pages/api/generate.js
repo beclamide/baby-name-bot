f745b728cfcebf5e56ea9de92c40e23b010bc089
John Bower <john@industrialassets.co.uk>
2022-10-20 15:40:15 +0100

    remove console log

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,7 +13,6 @@ export default async function (req, res) {
     max_tokens: 128
   });
   res.status(200).json({ result: completion.data.choices[0].text });
-  console.log(completion.data.choices);
 }
 
 function generatePrompt(sex, animal) {
